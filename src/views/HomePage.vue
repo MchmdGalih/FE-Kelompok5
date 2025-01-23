556ab41fd0166391e4e13dd79e6898811175c2ca
justpanca <pancafantastic6@gmail.com>
2025-01-24 01:14:29 +0700

    update category and search

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,19 +16,17 @@
     </div>
 
     <div class="py-4 px-10 mt-4">
-      <!-- Input untuk fitur pencarian -->
       <div class="mb-6">
         <input
           type="text"
           v-model="searchQuery"
-          class="w-1/2 px-4 py-2 border rounded-md"
-          placeholder="Search products..."
+          class="w-full px-4 py-2 border rounded-md"
+          placeholder="Cari Product Disini..."
         />
       </div>
 
       <h1 class="text-4xl font-bold text-black mb-4">Recommended for you</h1>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <!-- Tampilkan hasil pencarian -->
         <CardProduct :dataProps="filteredProducts" />
       </div>
     </div>
@@ -46,7 +44,7 @@ import CardProduct from "@/components/CardProduct.vue";
 const category = useCategoryStore();
 const product = useProductsStore();
 const isLoading = ref(true);
-const searchQuery = ref(""); // Kata kunci pencarian
+const searchQuery = ref("");
 
 const handleGetCategory = async () => {
   try {
@@ -70,7 +68,6 @@ const handleGetProduct = async () => {
   }
 };
 
-// Filter produk berdasarkan kata kunci pencarian
 const filteredProducts = computed(() => {
   if (!searchQuery.value) {
     return product.products;
